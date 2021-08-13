f1b907d204f3d63ca0163f834231e7c22f6c5dbb
reregi <woxieric@live.cn>
2021-08-13 17:42:40 +0800

    设置全局scss、文件夹别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { join } from "path";
 
+function resolve(dir) {
+  return join(__dirname, dir);
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve("src"),
+    },
+  },
+  css: {
+    //scss引入
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/common.scss";`,
+      },
+    },
+  },
+});
